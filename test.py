66ac62dc7666cc77dd5e489ac8652009820bea96
Dipti Bagal <52457642+BagalDipti@users.noreply.github.com>
2021-06-15 15:17:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import unittest
 import app
 
@@ -25,4 +24,11 @@ class TestHello(unittest.TestCase):
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
 if __name__ == '__main__':
+    import xmlrunner
+
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+
     unittest.main()
+
+
